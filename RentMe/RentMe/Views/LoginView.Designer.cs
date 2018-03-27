073ec49ac27a1bc69541c6a7dff9eef722d9568f
Diane Ela <dela1@my.westga.edu>
2018-03-27 13:14:54 -0400

    Added new RentMe project and designed login screen

---
+++ RentMe/RentMe/Views/LoginView.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace RentMe.Views
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginView));
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ttLogin = new System.Windows.Forms.ToolTip(this.components);
+            this.lblAuthorized = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.AccessibleDescription = "Enter your username";
+            this.txtLogin.AccessibleName = "Login";
+            this.txtLogin.AccessibleRole = System.Windows.Forms.AccessibleRole.ToolTip;
+            this.txtLogin.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.Location = new System.Drawing.Point(163, 132);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(214, 27);
+            this.txtLogin.TabIndex = 0;
+            this.txtLogin.Tag = "Enter your username";
+            this.ttLogin.SetToolTip(this.txtLogin, "Enter your username");
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLabel.Location = new System.Drawing.Point(110, 135);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(50, 19);
+            this.loginLabel.TabIndex = 1;
+            this.loginLabel.Text = "Login:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(85, 166);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 19);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(163, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 27);
+            this.textBox2.TabIndex = 2;
+            this.ttLogin.SetToolTip(this.textBox2, "Enter your password");
+            // 
+            // ttLogin
+            // 
+            this.ttLogin.AutomaticDelay = 100;
+            this.ttLogin.IsBalloon = true;
+            this.ttLogin.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.ttLogin.ToolTipTitle = "Tip!";
+            // 
+            // lblAuthorized
+            // 
+            this.lblAuthorized.AutoSize = true;
+            this.lblAuthorized.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuthorized.Location = new System.Drawing.Point(89, 52);
+            this.lblAuthorized.Name = "lblAuthorized";
+            this.lblAuthorized.Size = new System.Drawing.Size(335, 39);
+            this.lblAuthorized.TabIndex = 4;
+            this.lblAuthorized.Text = "Authorized Access Only!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(443, 23);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Access to this page is restricted to authorized users only.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(183, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblAuthorized);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtLogin);
+            this.Name = "LoginView";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ToolTip ttLogin;
+        private System.Windows.Forms.Label lblAuthorized;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ RentMe/RentMe/Views/LoginView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentMe.Views
+{
+    public partial class LoginView : Form
+    {
+        
+            public LoginView()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
