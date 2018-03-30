d6dd75616185684455d0fdfc79293726b6f4df2a
Brandon Williams <bwilli26@my.westga.edu>
2018-03-29 22:02:24 -0400

    updated DAL files

---
+++ RentMe/RentMe/DAL/RentMeDBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentMe.DAL
+{
+    class RentMeDBConnection
+    {
+        public static SqlConnection GetConnection()
+        {
+            string connectionString =
+                "Data Source=localhost;Initial Catalog=RentMe;" +
+                "Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
