9f057d694b5dd8c8972e61aeb0508f512efb7dbb
BWilliams <BWilliams@BWILLIAMS6530>
2018-04-16 12:02:07 -0400

    rental GUI functional

---
+++ RentMe/RentMe/Views/MemberReportView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentMe.Views
+{
+    public partial class MemberReportView : Form
+    {
+        public MemberReportView()
+        {
+            InitializeComponent();
+        }
+
+        private void MemberReportView_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'rentMeDataSet.Member' table. You can move, or remove it, as needed.
+            this.memberTableAdapter.Fill(this.rentMeDataSet.Member);
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
